30bbab9c2a85c1487ac43d296a9b5e9d21c03083
divyajyotiuk <divyajyoti.u@gmail.com>
2019-08-27 17:47:05 +0530

    register1 ui

---
+++ app/src/main/java/com/example/wutrial/RegisterActivity1.java
@@ -0,0 +1,15 @@
+package com.example.wutrial;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity1 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity_1);
+    }
+}
